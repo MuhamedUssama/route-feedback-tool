4123c6eeda68a40057752b2d5aa082976bbe3cc4
MuhamedUssama <mohamed.osama200538@gmail.com>
2025-12-28 16:10:44 +0200

    (refactor): separate main file to contain main funtion only

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,12 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:get_it/get_it.dart';
 import 'package:hive_ce_flutter/adapters.dart';
 import 'package:mentor_assistant/core/di/injection.dart';
-import 'package:mentor_assistant/features/settings/presentation/cubits/theme/theme_cubit.dart';
-import 'package:mentor_assistant/core/router/app_router.dart';
-import 'package:mentor_assistant/core/theme/app_theme.dart';
 import 'package:mentor_assistant/core/services/bloc_observer.dart';
 import 'package:mentor_assistant/features/settings/data/models/cycle_config_model.dart';
 import 'package:mentor_assistant/features/settings/data/models/group_config_model.dart';
+import 'package:mentor_assistant/mentor_assistant_app.dart';
 import 'package:window_manager/window_manager.dart';
 
 Future<void> main() async {
@@ -38,28 +35,4 @@ Future<void> main() async {
   await configureDependencies();
   Bloc.observer = MyBlocObserver();
   runApp(const MentorAssistant());
-}
-
-class MentorAssistant extends StatelessWidget {
-  const MentorAssistant({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => GetIt.I<ThemeCubit>(),
-      child: BlocBuilder<ThemeCubit, ThemeMode>(
-        builder: (context, themeMode) {
-          return MaterialApp(
-            title: 'Route Mentor Assistant',
-            debugShowCheckedModeBanner: false,
-            theme: AppTheme.lightTheme,
-            darkTheme: AppTheme.darkTheme,
-            themeMode: themeMode,
-            onGenerateRoute: AppRouter.onGenerateRoute,
-            initialRoute: AppRouter.splashRoute,
-          );
-        },
-      ),
-    );
-  }
 }
---
+++ lib/mentor_assistant_app.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
+import 'package:mentor_assistant/core/router/app_router.dart';
+import 'package:mentor_assistant/core/theme/app_theme.dart';
+import 'package:mentor_assistant/features/settings/presentation/cubits/theme/theme_cubit.dart';
+
+class MentorAssistant extends StatelessWidget {
+  const MentorAssistant({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => GetIt.I<ThemeCubit>(),
+      child: BlocBuilder<ThemeCubit, ThemeMode>(
+        builder: (context, themeMode) {
+          return MaterialApp(
+            title: 'Route Mentor Assistant',
+            debugShowCheckedModeBanner: false,
+            theme: AppTheme.lightTheme,
+            darkTheme: AppTheme.darkTheme,
+            themeMode: themeMode,
+            onGenerateRoute: AppRouter.onGenerateRoute,
+            initialRoute: AppRouter.splashRoute,
+          );
+        },
+      ),
+    );
+  }
+}
